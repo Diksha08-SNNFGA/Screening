c050e01f0097caea6e4c232bfeb20e1073c1a3b4
Diksha <drani60_phd20@thapar.edu>
2024-07-09 10:40:20 +0530

    update MM-PBSA

--- MM-PBSA files/run_mmpbsa.sh
+++ MM-PBSA files/run_mmpbsa.sh
@@ -1,1 +1,1 @@
-echo 19 20 |g_mmpbsa -f final.xtc -s md_0_1.tpr -n index.ndx -i mmpbsa.mdp -pdie 2 -pbsa -decomp -b 0 -e 20000
+echo 19 20 |g_mmpbsa -f final.xtc -s md_0_1.tpr -n index.ndx -i mmpbsa.mdp -pdie 2 -pbsa -decomp -b 15000 -e 20000
